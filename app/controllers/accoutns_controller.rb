546dffd7898aa3c20308f0930bb1340c8929a47f
soohoo05 <tyler.soohoo@gmail.com>
2018-10-14 14:56:34 -0400

    Done.

---
+++ app/controllers/accoutns_controller.rb
@@ -0,0 +1,22 @@
+class AccoutnsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
